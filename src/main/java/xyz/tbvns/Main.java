ceaa9f8638c23243b0ed94c4ea4639a2c6df3d95
Wyndev <108705186+WyndevCodes@users.noreply.github.com>
2024-08-09 18:09:55 -0500

    Moved player initializers to a PlayerListener class

--- src/main/java/xyz/tbvns/Main.java
+++ src/main/java/xyz/tbvns/Main.java
@@ -1,48 +1,22 @@
 package xyz.tbvns;
 
 import net.minestom.server.MinecraftServer;
-import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
-import net.minestom.server.event.player.PlayerSkinInitEvent;
-import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.utils.mojang.MojangUtils;
 import xyz.tbvns.item.ItemListener;
-import xyz.tbvns.item.ServerItem;
-
-import java.io.IOException;
-import java.util.UUID;
+import xyz.tbvns.player.PlayerListener;
 
 public class Main {
+    public static Instance lobbyInstance;
+
     public static void main(String[] args) {
         MinecraftServer minecraftServer = MinecraftServer.init();
 
-        Instance instance = MinecraftServer.getInstanceManager().createInstanceContainer();
+        lobbyInstance = MinecraftServer.getInstanceManager().createInstanceContainer();
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
 
-        //load skin from mojang api
-        globalEventHandler.addListener(PlayerSkinInitEvent.class, event -> {
-            try {
-                UUID mojangUUID = MojangUtils.getUUID(event.getPlayer().getUsername());
-                PlayerSkin skin = PlayerSkin.fromUuid(mojangUUID.toString());
-                event.setSkin(skin);
-            } catch (IOException e) {
-                e.printStackTrace();
-                //log.warn("Player {}'s skin failed to load!", event.getPlayer().getUsername());
-            }
-        });
-
-        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-            event.setSpawningInstance(instance);
-        });
-        globalEventHandler.addListener(PlayerSpawnEvent.class, event -> {
-            if (event.getInstance().equals(instance)) {
-                event.getPlayer().getInventory().clear();
-                event.getPlayer().getInventory().setItemStack(8, ServerItem.MENU.buildItem());
-            }
-        });
-
+        //set up listeners
+        new PlayerListener(globalEventHandler);
         new ItemListener(globalEventHandler);
 
         minecraftServer.start("0.0.0.0", 25565);
---
+++ src/main/java/xyz/tbvns/player/PlayerListener.java
@@ -0,0 +1,53 @@
+package xyz.tbvns.player;
+
+import net.minestom.server.entity.PlayerSkin;
+import net.minestom.server.event.EventFilter;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerSkinInitEvent;
+import net.minestom.server.event.player.PlayerSpawnEvent;
+import net.minestom.server.event.trait.PlayerEvent;
+import net.minestom.server.utils.mojang.MojangUtils;
+import xyz.tbvns.Main;
+import xyz.tbvns.item.ServerItem;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * Listener class for basic player events.
+ */
+public class PlayerListener {
+
+    public PlayerListener(GlobalEventHandler globalEventHandler) {
+        //create item event node
+        EventNode<PlayerEvent> node = EventNode.type("player-listener", EventFilter.PLAYER);
+
+        //load skin from mojang api
+        globalEventHandler.addListener(PlayerSkinInitEvent.class, event -> {
+            try {
+                UUID mojangUUID = MojangUtils.getUUID(event.getPlayer().getUsername());
+                PlayerSkin skin = PlayerSkin.fromUuid(mojangUUID.toString());
+                event.setSkin(skin);
+            } catch (IOException e) {
+                e.printStackTrace();
+                //log.warn("Player {}'s skin failed to load!", event.getPlayer().getUsername());
+            }
+        });
+
+        //set up player on server join
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            event.setSpawningInstance(Main.lobbyInstance);
+        });
+        globalEventHandler.addListener(PlayerSpawnEvent.class, event -> {
+            if (event.getInstance().equals(Main.lobbyInstance)) {
+                event.getPlayer().getInventory().clear();
+                event.getPlayer().getInventory().setItemStack(8, ServerItem.MENU.buildItem());
+            }
+        });
+
+        globalEventHandler.addChild(node);
+    }
+
+}
