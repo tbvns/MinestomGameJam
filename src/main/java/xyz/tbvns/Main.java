b40146e85bcf988648088407acc0d7b0970175e7
tbvns <tbvns601@gmail.com>
2024-08-09 11:37:38 +0000

    added empty server

--- src/main/java/xyz/tbvns/Main.java
+++ src/main/java/xyz/tbvns/Main.java
@@ -1,7 +1,20 @@
 package xyz.tbvns;
 
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.Instance;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        minecraftServer.start("0.0.0.0", 25565);
+
+        Instance instance = MinecraftServer.getInstanceManager().createInstanceContainer();
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            event.setSpawningInstance(instance);
+        });
     }
 }
