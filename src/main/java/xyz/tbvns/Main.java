c07665b0f7d4e84fb4782ee0e51d09e377c8ff2d
____tbvns____ <tbvns601@gmail.com>
2024-08-10 02:34:08 +0200

    Turned RedGun into a laser (insta kill everything in line of sight)

--- src/main/java/xyz/tbvns/Main.java
+++ src/main/java/xyz/tbvns/Main.java
@@ -1,8 +1,11 @@
 package xyz.tbvns;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.LivingEntity;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.block.Block;
 import xyz.tbvns.item.ItemListener;
 import xyz.tbvns.player.PlayerListener;
 
@@ -17,6 +20,10 @@ public class Main {
 
         lobbyInstance = MinecraftServer.getInstanceManager().createInstanceContainer();
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+
+        LivingEntity cow = new LivingEntity(EntityType.COW);
+        cow.setInstance(lobbyInstance);
+        lobbyInstance.setBlock(0, -20, 0, Block.STONE);
 
         //set up listeners
         new PlayerListener(globalEventHandler);
--- src/main/java/xyz/tbvns/item/custom/RedGun.java
+++ src/main/java/xyz/tbvns/item/custom/RedGun.java
@@ -1,10 +1,18 @@
 package xyz.tbvns.item.custom;
 
+import net.minestom.server.event.player.PlayerUseItemEvent;
 import net.minestom.server.item.Material;
 import xyz.tbvns.game.Color;
+
+import java.util.Objects;
 
 public class RedGun extends ColorGun {
     public RedGun() {
         super(Color.RED, Material.LEATHER_HORSE_ARMOR);
+    }
+
+    @Override
+    public void onClick(PlayerUseItemEvent event) {
+        Objects.requireNonNull(event.getPlayer().getLineOfSightEntity(10, Objects::nonNull)).remove();
     }
 }
